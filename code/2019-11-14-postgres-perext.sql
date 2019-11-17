41b813fc90c45e491c87c8a12ca6a8c35a417e99
Brian Labelle <brianjlabelle@yahoo.com>
2019-11-17 16:43:37 -0600

    Update 2019-11-14-postgres-perext.sql

--- code/2019-11-14-postgres-perext.sql
+++ code/2019-11-14-postgres-perext.sql
@@ -4,7 +4,6 @@
 
 CREATE TABLE public.perext
 (
-    "UniqueID" bigint NOT NULL,
     "LoanIdentifier" bigint,
     "MonthlyReportingPeriod" date,
     "LoanAge" smallint,
@@ -14,7 +13,6 @@ CREATE TABLE public.perext
     "MetropolitanStatisticalAreaMSA" bigint,
     "CurrentLoanDelinquencyStatus" character varying(15) COLLATE pg_catalog."default",
     "ForeclosureDate" character varying(25) COLLATE pg_catalog."default",
-    CONSTRAINT perext_pkey PRIMARY KEY ("UniqueID"),
     CONSTRAINT "LoanIdentifier" FOREIGN KEY ("LoanIdentifier")
         REFERENCES public.acq ("LoanIdentifier") MATCH SIMPLE
         ON UPDATE NO ACTION
